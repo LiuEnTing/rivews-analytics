54159cfa6bb36f58dd10547d478e0e2dbb7b740f
LiuEnTing <4a2g0005@stust.edu.tw>
2020-08-20 21:52:11 +0800

    new rivews-analytics

--- read.py
+++ read.py
@@ -14,5 +14,15 @@ for d in data:
 	sum_len += len(d)
 print('留言平均長度', sum_len/len(data))
 
+#篩選
+new = []
+for d in data:
+	if len(d) < 200:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於200')
 
-
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆當中有說到good')
